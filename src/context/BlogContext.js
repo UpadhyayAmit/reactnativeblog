668291aa03557790db78ebb4f6df892655536b67
Amit Upadhyay <devcoder.blog@outlook.com>
2022-07-07 23:25:19 +0530

    Advance Context and provider

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,27 +1,23 @@
-import React, { useReducer } from 'react';
+import createDataContext from './createDataContext';
 
-const BlogContext = React.createContext();
-export const BlogProvider = ({ children }) => {
-  const blogReducer = (state, action) => {
-    switch (action.type) {
-      case 'add_blogpost':
-        return [...state, { title: `blog post #${state.length + 1}` }];
-      default:
-        return state;
-    }
-  };
-
-  const [blogPosts, dispatch] = useReducer(blogReducer, []);
-
-  const addBlogPost = () => {
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_blogpost':
+      return [...state, { title: `blog post #${state.length + 1}` }];
+    default:
+      return state;
+  }
+};
+const addBlogPost = (dispatch) => {
+  return () => {
     dispatch({ type: 'add_blogpost' });
   };
-
-  return (
-    <BlogContext.Provider value={{ data: blogPosts, addBlogPost: addBlogPost }}>
-      {children}
-    </BlogContext.Provider>
-  );
 };
 
-export default BlogContext;
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  {
+    addBlogPost: addBlogPost,
+  },
+  []
+);
